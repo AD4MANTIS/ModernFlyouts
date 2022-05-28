701b89e37dec052cf32c403937a9851f62e277bb
AD4MANTIS <31745687+AD4MANTIS@users.noreply.github.com>
2022-05-28 12:22:55 +0200

    UI Fix for Media without Album

---
+++ ModernFlyouts/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ModernFlyouts.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = false;
+
+            if (value is string text)
+                result = !string.IsNullOrEmpty(text);
+
+            return result ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
